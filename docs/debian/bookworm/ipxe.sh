eb1c394f23f2f892e0ba2a894cdb96241b2cfa5d
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2023-11-04 11:41:02 -0400

    Remove boot parameters

--- docs/debian/bookworm/ipxe.sh
+++ docs/debian/bookworm/ipxe.sh
@@ -7,6 +7,6 @@ set preseed_url https://tlhakhan.github.io/virtualbox-builder/debian/bookworm/pr
 set linux_url http://deb.debian.org/debian/dists/bookworm/main/installer-amd64/current/images/netboot/debian-installer/amd64/linux
 set initrd_url http://deb.debian.org/debian/dists/bookworm/main/installer-amd64/current/images/netboot/debian-installer/amd64/initrd.gz
 
-kernel ${linux_url} auto=true priority=critical interface=auto ipv6.disable=1 preseed/url=${preseed_url} DEBIAN_FRONTEND=text net.ifnames=0
+kernel ${linux_url} auto=true priority=critical interface=auto preseed/url=${preseed_url} DEBIAN_FRONTEND=text
 initrd ${initrd_url}
 boot
