ef8b27c59abc01e2e6029d647291df3068683f4c
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2023-11-04 09:47:18 -0400

    Add Debian bookworm configuration

---
+++ docs/debian/bookworm/ipxe.sh
@@ -0,0 +1,12 @@
+#!ipxe
+#
+# Debian Installer
+#
+
+set preseed_url https://tlhakhan.github.io/virtualbox-buider/debian/bookworm/preseed.cfg
+set linux_url http://deb.debian.org/debian/dists/bookworm/main/installer-amd64/current/images/netboot/debian-installer/amd64/linux
+set initrd_url http://deb.debian.org/debian/dists/bookworm/main/installer-amd64/current/images/netboot/debian-installer/amd64/initrd.gz
+
+kernel ${linux_url} auto=true priority=critical interface=auto ipv6.disable=1 preseed/url=${preseed_url} DEBIAN_FRONTEND=text net.ifnames=0
+initrd ${initrd_url}
+boot
